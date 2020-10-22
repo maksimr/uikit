5e8490d6d7a2dd690433f46cb930923320a6f20e
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-10-22 15:49:04 +0300

    chore: seupt react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { render } from 'react-dom';
+import { Popup } from '../lib/react/Popup';
+
 function main() {
-  document.getElementById('app').innerHTML = 'Hello World!';
+  const rootNode = document.getElementById('app');
+  rootNode.style.position = 'relative';
+
+  render(<App/>, rootNode);
+
+  function App() {
+    const [anchorNode, setAnchorNode] = useState(null);
+    const [img, setImg] = useState(null);
+    useEffect(() => {
+      const id = setTimeout(() => {
+        setImg('https://picsum.photos/100/100?grayscale');
+      }, 1000);
+      return () => clearTimeout(id);
+    }, [setImg]);
+
+    return (
+      <div>
+        <button
+          style={{position: 'fixed', bottom: 0}}
+          ref={(node) => setAnchorNode(node)}
+        >
+          Popup
+        </button>
+        {anchorNode ? (
+          <Popup anchorNode={anchorNode} resizable={true} parentNode={rootNode}>
+            <div>
+              Hello World!
+              <div>{img ? <img alt="pic" src={img}/> : null}</div>
+            </div>
+          </Popup>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 main();
