0ccdf8ce5503c0fc1dd819777290a03f66218529
maksimr <maksimr@users.noreply.github.com>
2020-11-08 13:16:06 +0000

    deploy: 6774fc4439c624beec7e85959f2935421c2b477e

--- react/ClickZone.test.js
+++ react/ClickZone.test.js
@@ -1,24 +1,34 @@
 import { act } from 'react-dom/test-utils';
-import { render } from 'react-dom';
+import { render, createPortal } from 'react-dom';
 import React from 'react';
 import { ClickZone } from './ClickZone';
-describe.only('ClickZone', function () {
+describe('ClickZone', function () {
     let rootNode;
     let onClickAway;
+    let portalNode;
     beforeEach(function () {
+        portalNode = document.createElement('div');
         rootNode = document.createElement('div');
         onClickAway = jest.fn();
         act(() => {
             render(React.createElement(ClickZone, { onClickAway: onClickAway },
-                React.createElement("div", { id: "foo" }, "Hello World!")), rootNode);
+                React.createElement("div", { id: "foo" },
+                    React.createElement(UsePortal, null))), rootNode);
         });
+        function UsePortal() {
+            return createPortal(React.createElement("div", { id: "portal" }), portalNode);
+        }
     });
-    it('should click away zone', function () {
+    it('should handle click away zone', function () {
         click(rootNode);
         expect(onClickAway).toHaveBeenCalled();
     });
     it('should not fire click away if we click inside zone', () => {
         click(rootNode.querySelector('#foo'));
+        expect(onClickAway).not.toHaveBeenCalled();
+    });
+    it('should not fire click inside portal which in the same virual dom tree', () => {
+        click(portalNode.querySelector('#portal'));
         expect(onClickAway).not.toHaveBeenCalled();
     });
     function click(node) {
