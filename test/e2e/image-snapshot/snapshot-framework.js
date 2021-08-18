202b23c35f79396b2e1e13d04fd5fe660bf04968
Maksim Ryzhikov <rv.maksim@gmail.com>
2021-08-18 16:39:49 +0000

    chore: rename

--- test/e2e/image-snapshot/snapshot-framework.js
+++ test/e2e/image-snapshot/snapshot-framework.js
@@ -2,7 +2,7 @@ module.exports = {
   'framework:snapshot-jasmine': ['factory', function(/* config.files */ files) {
     files.unshift(
       {
-        pattern: require.resolve('./snapshot-client.js'),
+        pattern: require.resolve('./snapshot.js'),
         included: true,
         served: true,
         watched: false
