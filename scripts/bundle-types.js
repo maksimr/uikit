d68f5a0f824b791d2fcc34c6555c58fe8f5a3a61
Maksim Ryzhikov <rv.maksim@gmail.com>
2022-02-20 16:09:36 +0000

    chore: fix bundle types

--- scripts/bundle-types.js
+++ scripts/bundle-types.js
@@ -1,22 +1,21 @@
 async function main() {
-  const fs = require('fs').promises;
+  const { writeFile, readFile, readdir, lstat } = require('fs/promises');
   const { name: pkgName } = require('../package.json');
   const [inputDir, outputDir = 'dist'] = process.argv.slice(2);
   const bundlePath = require('path').join(outputDir, 'index.d.ts');
 
   const types = await findTypeFiles();
   const typesBundle = await bundleTypes(types);
-  await fs.writeFile(bundlePath, typesBundle);
+  await writeFile(bundlePath, typesBundle);
 
   global.console.log('Types successfully bundle');
-  return;
 
   async function findTypeFiles() {
     return await readdirRecursive(inputDir, /\.d\.ts$/);
   }
 
   /**
-   * @param {string[]} types 
+   * @param {string[]} types
    * @returns {Promise<string>}
    */
   async function bundleTypes(types) {
@@ -24,29 +23,51 @@ async function main() {
   }
 
   /**
-   * @param {string} typeFile 
+   * @param {string} typeFile
    * @returns {Promise<string>}
    */
   async function wrapTypeInModule(typeFile) {
-    const code = await fs.readFile(typeFile);
+    const code = await readFile(typeFile);
     const module = typeFile.replace(`${inputDir}/`, '').replace(/\.d\.ts$/, '');
-    return `declare module "${pkgName}/${module}" {\n${code}}`;
+    const processedCode = code.toString()
+      .replace(/(import\(["|'])([^"']+)(["|']\))/g, fixLocalDependencyPath)
+      .replace(/(\s+from\s+["|'])([^"']+)(["|'];)/g, fixLocalDependencyPath);
+    return `declare module "${pkgName}/${module}" {\n${processedCode}}`;
+
+    /**
+     * @param {string} _ 
+     * @param {string} prefix 
+     * @param {string} modulePath 
+     * @param {string} postfix 
+     * @returns {string}
+     */
+    function fixLocalDependencyPath(_, prefix, modulePath, postfix) {
+      if (/^(\.|\/)/.test(modulePath)) {
+        const moduleDir = require('path').dirname(module);
+        const relModulePath = require('path').join(moduleDir, modulePath);
+        return `${prefix}${pkgName}/${relModulePath}${postfix}`;
+      }
+      return _;
+    }
   }
 
   /**
-   * @param {string} dir 
-   * @param {RegExp} filter 
+   * @param {string} dir
+   * @param {RegExp} filter
    * @returns {Promise<string[]>}
    */
   async function readdirRecursive(dir, filter) {
-    const dirFiles = await fs.readdir(dir);
+    const dirFiles = await readdir(dir);
     const allFiles = await Promise.all(dirFiles.map(async (fileName) => {
       const path = require('path').join(dir, fileName);
-      const isDirectory = (await fs.lstat(path)).isDirectory();
+      const isDirectory = (await lstat(path)).isDirectory();
       return isDirectory ? readdirRecursive(path, filter) : path;
     }));
     return allFiles.flat(1).filter((it) => filter.test(it));
   }
-}
+};
 
-main();
+module.exports = main;
+if (require.main === module) {
+  main();
+}
